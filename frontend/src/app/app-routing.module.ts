62bb68d2e4f05ac24b830ed9e86663d5de7d076e
wislla <wislla21@gmail.com>
2020-10-13 09:06:22 -0300

    add selected

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ScheduleComponent } from './components/schedule/schedule.component';
-import { SelectionCollectionComponent } from './components/selection-collection/selection-collection.component';
+import { ScheduleComponent } from './pages/schedule/schedule.component';
+import { SelectedComponent } from './pages/selection-collection/selected/selected.component';
+import { SelectionCollectionComponent } from './pages/selection-collection/selection-collection.component';
 
 const routes: Routes = [
   { path: '', component: ScheduleComponent },
-  { path: 'selection', component: SelectionCollectionComponent }
+  { path: 'selection', component: SelectionCollectionComponent }, //app-selected
+  { path: 'selected', component: SelectedComponent }
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar'; 
 import {MatIconModule} from '@angular/material/icon'; 
 import {MatTabsModule} from '@angular/material/tabs';
-import { ScheduleComponent } from './components/schedule/schedule.component'; 
+import { ScheduleComponent } from './pages/schedule/schedule.component'; 
 import {MatGridListModule} from '@angular/material/grid-list';
 import { FullCalendarModule } from '@fullcalendar/angular'; // the main connector. must go first
 import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin
@@ -17,7 +17,7 @@ import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'; // a plugin
 import listPlugin from '@fullcalendar/list';
 import {MatDialogModule} from '@angular/material/dialog';
-import { DialogComponent } from './components/home/dialog/dialog.component'
+import { DialogComponent } from './pages/home/dialog/dialog.component'
 import {MatInputModule} from '@angular/material/input';
 import { MatNativeDateModule } from '@angular/material/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -33,7 +33,9 @@ import{MatCheckboxModule} from '@angular/material/checkbox';
 import {MatSidenavModule} from '@angular/material/sidenav'; 
 import {MatExpansionModule} from '@angular/material/expansion'; 
 import {MatListModule} from '@angular/material/list';
-import { SelectionCollectionComponent } from './components/selection-collection/selection-collection.component'; 
+import { SelectionCollectionComponent } from './pages/selection-collection/selection-collection.component';
+import { SelectedComponent } from './pages/selection-collection/selected/selected.component'; 
+import {MatCardModule} from '@angular/material/card'; 
 
 FullCalendarModule.registerPlugins([ // register FullCalendar plugins
   dayGridPlugin,
@@ -48,7 +50,8 @@ FullCalendarModule.registerPlugins([ // register FullCalendar plugins
     HomeComponent,
     ScheduleComponent,
     DialogComponent,
-    SelectionCollectionComponent
+    SelectionCollectionComponent,
+    SelectedComponent
   ],
   imports: [
     BrowserModule,
@@ -75,6 +78,7 @@ FullCalendarModule.registerPlugins([ // register FullCalendar plugins
     MatExpansionModule,
     MatListModule,
     MatChipsModule,
+    MatCardModule,
     
     
   ],
