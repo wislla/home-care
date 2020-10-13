3d451ee40a1582758cc5fb68db2f82f387ab8934
wislla <wislla21@gmail.com>
2020-10-13 11:58:06 -0300

    add

--- frontend/src/app/pages/selection-collection/selected/selected.component.ts
+++ frontend/src/app/pages/selection-collection/selected/selected.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-selected',
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./selected.component.css']
 })
 export class SelectedComponent implements OnInit {
+ 
 
+  
   constructor() { }
 
   ngOnInit(): void {
