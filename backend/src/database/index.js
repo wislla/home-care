a9621b26921cabf8ad3ce971c94559d95a53ed7d
wislla <wislla21@gmail.com>
2021-07-09 14:16:51 -0300

    delee

--- backend/src/database/index.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require ('sequelize');
-const User = require('../models/User');
-const dbConfig = require('./../config/database');
-const Collect = require ('./../models/Collect')
-
-const connection = new Sequelize(dbConfig);
-Collect.init(connection);
-User.init(connection);
-module.exports = connection;
